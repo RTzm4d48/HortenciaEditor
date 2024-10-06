409f7a4c32d261f94f0ae80a8f7be83ecd370c64
RTzm4d48 <aguirre.sanchez.as@gmail.com>
2024-10-06 00:32:34 -0500

    [feat] - H.E #1

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+from django.http import HttpResponse
+
+# Create your views here.
+def mi_vista(request):
+    return render(request, 'index.html')
